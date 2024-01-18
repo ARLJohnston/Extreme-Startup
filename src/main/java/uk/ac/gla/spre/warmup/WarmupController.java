320d760fe8d5e6b8b63479cc65e061c474ff35fc
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-01-18 13:25:16 +0000

    feat: add WarmupController

--- src/main/java/uk/ac/gla/spre/warmup/WarmupController.java
+++ src/main/java/uk/ac/gla/spre/warmup/WarmupController.java
@@ -1,4 +1,13 @@
 package uk.ac.gla.spre.warmup;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
 public class WarmupController {
+		@GetMapping("/")
+		public String hello(@RequestParam Map<String,String> allParams) {
+				System.out.println("Request received! "+allParams.entrySet());
+				return "Hello World!";
+		}
 }
