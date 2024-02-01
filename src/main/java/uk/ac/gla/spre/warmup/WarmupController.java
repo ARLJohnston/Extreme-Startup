285ea2dc1f068c78902e31b66d3eb905c78ebf60
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-02-01 14:31:56 +0000

    feat: add proper logging to service

--- src/main/java/uk/ac/gla/spre/warmup/WarmupController.java
+++ src/main/java/uk/ac/gla/spre/warmup/WarmupController.java
@@ -1,13 +1,17 @@
 package uk.ac.gla.spre.warmup;
 
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Map;
 
 @RestController
 public class WarmupController {
+		private static Logger logger = LoggerFactory.getLogger(WarmupController.class);
+
 		@GetMapping("/")
 		public String hello(@RequestParam Map<String,String> allParams) {
-				System.out.println("Request received! "+allParams.entrySet());
+				logger.info("GET: " +allParams.entrySet());
 				return "Alistair";
 		}
 }
